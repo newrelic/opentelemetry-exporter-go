62d67b8111bba0a8a49e9f67757d4fa8016f2d48
will@newrelic.com <will@newrelic.com>
2019-12-10 20:16:25 -0800

    Initial prototype implementation

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/newrelic/newrelic-opentelemetry-exporter-go
+
+go 1.13
+
+require (
+	go.opentelemetry.io/otel v0.2.0
+	github.com/newrelic/newrelic-telemetry-sdk-go v0.1.0
+)
---
+++ newrelic/exporter.go
@@ -0,0 +1,107 @@
+// Package newrelic contains an OpenTelemetry tracing exporter for New Relic.
+package newrelic
+
+import (
+	"context"
+	"encoding/hex"
+
+	"github.com/newrelic/newrelic-telemetry-sdk-go/telemetry"
+	"go.opentelemetry.io/otel/api/core"
+	"go.opentelemetry.io/otel/sdk/export/trace"
+)
+
+// Java implementation:
+// https://github.com/newrelic/newrelic-opentelemetry-java-exporters/tree/master/src/main/java/com/newrelic/telemetry/opentelemetry/export
+
+// Exporter exports spans to New Relic.
+type Exporter struct {
+	harvester *telemetry.Harvester
+	// serviceName is the name of this service or application.
+	serviceName string
+	// IgnoreStatusCodes controls which SpanData.Status
+	// (https://godoc.org/google.golang.org/grpc/codes) codes are turned into
+	// errors on Spans.  A Span with a Status greater than 0 that is not
+	// in this slice will be marked as an error.  When instantiated with
+	// NewExporter this field defaults to only include 5 (NOT_FOUND).
+	IgnoreStatusCodes []uint32
+}
+
+// NewExporter creates a new Exporter that exports spans to New Relic.
+func NewExporter(serviceName, apiKey string, options ...func(*telemetry.Config)) (*Exporter, error) {
+	options = append([]func(*telemetry.Config){telemetry.ConfigAPIKey(apiKey)}, options...)
+	h, err := telemetry.NewHarvester(options...)
+	if nil != err {
+		return nil, err
+	}
+	return &Exporter{
+		harvester:         h,
+		serviceName:       serviceName,
+		IgnoreStatusCodes: []uint32{5},
+	}, nil
+}
+
+var (
+	_ interface {
+		trace.SpanSyncer
+		trace.SpanBatcher
+	} = &Exporter{}
+)
+
+// ExportSpans exports multiple spans to New Relic.
+func (e *Exporter) ExportSpans(ctx context.Context, spans []*trace.SpanData) {
+	for _, s := range spans {
+		e.ExportSpan(ctx, s)
+	}
+}
+
+// ExportSpan exports a span to New Relic.
+func (e *Exporter) ExportSpan(ctx context.Context, span *trace.SpanData) {
+	e.harvester.RecordSpan(e.transformSpan(span))
+}
+
+func (e *Exporter) responseCodeIsError(code uint32) bool {
+	if code == 0 {
+		return false
+	}
+	for _, ignoreCode := range e.IgnoreStatusCodes {
+		if code == ignoreCode {
+			return false
+		}
+	}
+	return true
+}
+
+func transformSpanID(id core.SpanID) string {
+	if !id.IsValid() {
+		return ""
+	}
+	return hex.EncodeToString(id[:])
+}
+
+func (e *Exporter) makeAttributes(span *trace.SpanData) map[string]interface{} {
+	attributes := make(map[string]interface{}, len(span.Attributes)+1)
+	for _, pair := range span.Attributes {
+		attributes[string(pair.Key)] = pair.Value.AsInterface()
+	}
+
+	if e.responseCodeIsError(uint32(span.Status)) {
+		attributes["error.message"] = span.Status.String()
+	}
+
+	return attributes
+}
+
+// https://godoc.org/github.com/newrelic/newrelic-telemetry-sdk-go/telemetry#Span
+// https://godoc.org/go.opentelemetry.io/otel/sdk/export/trace#SpanData
+func (e *Exporter) transformSpan(span *trace.SpanData) telemetry.Span {
+	return telemetry.Span{
+		ID:          span.SpanContext.SpanIDString(),
+		TraceID:     span.SpanContext.TraceIDString(),
+		Timestamp:   span.StartTime,
+		Name:        span.Name,
+		ParentID:    transformSpanID(span.ParentSpanID),
+		Duration:    span.EndTime.Sub(span.StartTime),
+		ServiceName: e.serviceName,
+		Attributes:  e.makeAttributes(span),
+	}
+}
