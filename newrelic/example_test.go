6586c0322d09801da40ae24fc779237df62b7207
Tyler Yahn <MrAlias@users.noreply.github.com>
2020-09-10 07:44:08 -0700

    Add pipeline setup functions (#20)
    * Add pipeline setup functions
    
    The `NewExportPipeline` function configures an Exporter from environment
    variables and build configurable trace and metric pipelines with the
    exporter. The built `trace.Provider` and metric `Controller` are
    returned.
    
    The `InstallNewPipeline` function configures an Exporter from
    environment variables and installs it into a telemetry pipeline using
    default configuration as well as setting it as the global `*Provider`.
    
    The examples for the package are updated to show how these functions can
    be used.
    
    * Feedback
    
    Update docs
    
    * Upgrade to v0.11.0

--- newrelic/example_test.go
+++ newrelic/example_test.go
@@ -6,24 +6,17 @@ package newrelic_test
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/newrelic/newrelic-telemetry-sdk-go/telemetry"
 	"github.com/newrelic/opentelemetry-exporter-go/newrelic"
 	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/label"
+	"go.opentelemetry.io/otel/sdk/metric/controller/push"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/semconv"
 )
-
-func Example() {
-	exporter, err := newrelic.NewExporter("My Service", os.Getenv("NEW_RELIC_API_KEY"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	tp, err := trace.NewProvider(trace.WithSyncer(exporter))
-	if err != nil {
-		log.Fatal(err)
-	}
-	global.SetTraceProvider(tp)
-}
 
 func ExampleNewExporter() {
 	// To enable Infinite Tracing on the New Relic Edge, use the
@@ -42,4 +35,49 @@ func ExampleNewExporter() {
 		log.Fatal(err)
 	}
 	global.SetTraceProvider(tp)
+}
+
+func ExampleNewExportPipeline() {
+	// Include environment in resource.
+	r := resource.New(
+		label.String("environment", "production"),
+		semconv.ServiceNameKey.String("My Service"),
+	)
+
+	// Assumes the NEW_RELIC_API_KEY environment variable contains your New
+	// Relic Insights insert API key. This will error if it does not.
+	traceProvider, controller, err := newrelic.NewExportPipeline(
+		"My Service",
+		[]trace.ProviderOption{
+			trace.WithConfig(trace.Config{
+				// Conservative sampler.
+				DefaultSampler: trace.ParentSample(trace.NeverSample()),
+				// Reduce span events.
+				MaxEventsPerSpan: 10,
+				Resource:         r,
+			}),
+		},
+		[]push.Option{
+			// Increase push frequency.
+			push.WithPeriod(time.Second),
+			push.WithResource(r),
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer controller.Stop()
+
+	global.SetTraceProvider(traceProvider)
+	global.SetMeterProvider(controller.Provider())
+}
+
+func ExampleInstallNewPipeline() {
+	// Assumes the NEW_RELIC_API_KEY environment variable contains your New
+	// Relic Insights insert API key. This will error if it does not.
+	controller, err := newrelic.InstallNewPipeline("My Service")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer controller.Stop()
 }
--- newrelic/exporter.go
+++ newrelic/exporter.go
@@ -1,28 +1,35 @@
 // Copyright 2019 New Relic Corporation. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// Package newrelic contains an OpenTelemetry tracing exporter for New Relic.
+// Package newrelic provides an OpenTelemetry exporter for New Relic.
 package newrelic
 
 import (
 	"context"
 	"errors"
+	"os"
+
+	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/metric"
+	apitrace "go.opentelemetry.io/otel/api/trace"
 	"go.opentelemetry.io/otel/sdk/export/metric/aggregation"
+	"go.opentelemetry.io/otel/sdk/metric/controller/push"
+	"go.opentelemetry.io/otel/sdk/metric/processor/basic"
+	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/semconv"
 
 	"github.com/newrelic/newrelic-telemetry-sdk-go/telemetry"
 	"github.com/newrelic/opentelemetry-exporter-go/newrelic/internal/transform"
-	metricsdk "go.opentelemetry.io/otel/sdk/export/metric"
-	tracesdk "go.opentelemetry.io/otel/sdk/export/trace"
+	exportmetric "go.opentelemetry.io/otel/sdk/export/metric"
+	exporttrace "go.opentelemetry.io/otel/sdk/export/trace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
 const (
 	version          = "0.1.0"
 	userAgentProduct = "NewRelic-Go-OpenTelemetry"
 )
-
-// Java implementation:
-// https://github.com/newrelic/newrelic-opentelemetry-java-exporters/tree/master/src/main/java/com/newrelic/telemetry/opentelemetry/export
 
 // Exporter exports OpenTelemetry data to New Relic.
 type Exporter struct {
@@ -35,9 +42,9 @@ var (
 	errServiceNameEmpty = errors.New("service name is required")
 )
 
-// NewExporter creates a new Exporter that exports spans to New Relic.
-func NewExporter(serviceName, apiKey string, options ...func(*telemetry.Config)) (*Exporter, error) {
-	if serviceName == "" {
+// NewExporter creates a new Exporter that exports telemetry to New Relic.
+func NewExporter(service, apiKey string, options ...func(*telemetry.Config)) (*Exporter, error) {
+	if service == "" {
 		return nil, errServiceNameEmpty
 	}
 	options = append([]func(*telemetry.Config){
@@ -53,25 +60,122 @@ func NewExporter(serviceName, apiKey string, options ...func(*telemetry.Config))
 	}
 	return &Exporter{
 		harvester:   h,
-		serviceName: serviceName,
+		serviceName: service,
 	}, nil
 }
 
+// NewExportPipeline creates a new OpenTelemetry telemetry pipeline using a
+// New Relic Exporter configured with default setting. It is the callers
+// responsibility to stop the returned push Controller. This function uses the
+// following environment variables to configure the exporter installed in the
+// pipeline:
+//
+//    * `NEW_RELIC_API_KEY`: New Relic Insights insert API key.
+//    * `NEW_RELIC_METRIC_URL`: Override URL to New Relic metric endpoint.
+//    * `NEW_RELIC_TRACE_URL`: Override URL to New Relic trace endpoint.
+//
+// More information about the New Relic Insights insert API key can be found
+// here: https://docs.newrelic.com/docs/apis/get-started/intro-apis/types-new-relic-api-keys#event-insert-key.
+//
+// The exporter will send telemetry to the default New Relic metric and trace
+// API endpoints in the United States. These can be overwritten with the above
+// environment variables. These are useful if you wish to send to our EU
+// endpoints:
+//
+//    * EU metric API endpoint: metric-api.eu.newrelic.com/metric/v1
+//    * EU trace API endpoint: trace-api.eu.newrelic.com/trace/v1
+func NewExportPipeline(service string, traceOpt []sdktrace.ProviderOption, pushOpt []push.Option) (apitrace.Provider, *push.Controller, error) {
+	apiKey, ok := os.LookupEnv("NEW_RELIC_API_KEY")
+	if !ok {
+		return nil, nil, errors.New("missing New Relic API key")
+	}
+
+	var eOpts []func(*telemetry.Config)
+	if u, ok := os.LookupEnv("NEW_RELIC_METRIC_URL"); ok {
+		eOpts = append(eOpts, func(cfg *telemetry.Config) {
+			cfg.MetricsURLOverride = u
+		})
+	}
+	if u, ok := os.LookupEnv("NEW_RELIC_TRACE_URL"); ok {
+		eOpts = append(eOpts, telemetry.ConfigSpansURLOverride(u))
+	}
+
+	exporter, err := NewExporter(service, apiKey, eOpts...)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Minimally default resource with a service name. This is overwritten if
+	// another is passed in traceOpt or pushOpt.
+	r := resource.New(semconv.ServiceNameKey.String(service))
+
+	tp, err := sdktrace.NewProvider(
+		append([]sdktrace.ProviderOption{
+			sdktrace.WithSyncer(exporter),
+			sdktrace.WithResource(r),
+		},
+			traceOpt...)...,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pusher := push.New(
+		basic.New(simple.NewWithExactDistribution(), exporter),
+		exporter,
+		append([]push.Option{push.WithResource(r)}, pushOpt...)...,
+	)
+	pusher.Start()
+
+	return tp, pusher, nil
+}
+
+// InstallNewPipeline installs a New Relic exporter with default settings
+// in the global OpenTelemetry telemetry pipeline. It is the callers
+// responsibility to stop the returned push Controller. This function uses the
+// following environment variables to configure the exporter installed in the
+// pipeline:
+//
+//    * `NEW_RELIC_API_KEY`: New Relic Insights insert API key.
+//    * `NEW_RELIC_METRIC_URL`: Override URL to New Relic metric endpoint.
+//    * `NEW_RELIC_TRACE_URL`: Override URL to New Relic trace endpoint.
+//
+// More information about the New Relic Insights insert API key can be found
+// here: https://docs.newrelic.com/docs/apis/get-started/intro-apis/types-new-relic-api-keys#event-insert-key.
+//
+// The exporter will send telemetry to the default New Relic metric and trace
+// API endpoints in the United States. These can be overwritten with the above
+// environment variables. These are useful if you wish to send to our EU
+// endpoints:
+//
+//    * EU metric API endpoint: metric-api.eu.newrelic.com/metric/v1
+//    * EU trace API endpoint: trace-api.eu.newrelic.com/trace/v1
+func InstallNewPipeline(service string) (*push.Controller, error) {
+	tp, controller, err := NewExportPipeline(service, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	global.SetTraceProvider(tp)
+	global.SetMeterProvider(controller.Provider())
+	return controller, nil
+}
+
 var (
-	_ tracesdk.SpanSyncer  = (*Exporter)(nil)
-	_ tracesdk.SpanBatcher = (*Exporter)(nil)
-	_ metricsdk.Exporter   = (*Exporter)(nil)
+	_ exporttrace.SpanSyncer  = (*Exporter)(nil)
+	_ exporttrace.SpanBatcher = (*Exporter)(nil)
+	_ exportmetric.Exporter   = (*Exporter)(nil)
 )
 
 // ExportSpans exports multiple spans to New Relic.
-func (e *Exporter) ExportSpans(ctx context.Context, spans []*tracesdk.SpanData) {
+func (e *Exporter) ExportSpans(ctx context.Context, spans []*exporttrace.SpanData) {
 	for _, s := range spans {
 		e.ExportSpan(ctx, s)
 	}
 }
 
 // ExportSpan exports a span to New Relic.
-func (e *Exporter) ExportSpan(ctx context.Context, span *tracesdk.SpanData) {
+func (e *Exporter) ExportSpan(ctx context.Context, span *exporttrace.SpanData) {
 	if nil == e {
 		return
 	}
@@ -79,8 +183,8 @@ func (e *Exporter) ExportSpan(ctx context.Context, span *tracesdk.SpanData) {
 }
 
 // Export exports metrics to New Relic.
-func (e *Exporter) Export(_ context.Context, cps metricsdk.CheckpointSet) error {
-	return cps.ForEach(e, func(record metricsdk.Record) error {
+func (e *Exporter) Export(_ context.Context, cps exportmetric.CheckpointSet) error {
+	return cps.ForEach(e, func(record exportmetric.Record) error {
 		m, err := transform.Record(e.serviceName, record)
 		if err != nil {
 			return err
@@ -90,6 +194,6 @@ func (e *Exporter) Export(_ context.Context, cps metricsdk.CheckpointSet) error 
 	})
 }
 
-func (e *Exporter) ExportKindFor(_ *metric.Descriptor, _ aggregation.Kind) metricsdk.ExportKind {
-	return metricsdk.DeltaExporter
+func (e *Exporter) ExportKindFor(_ *metric.Descriptor, _ aggregation.Kind) exportmetric.ExportKind {
+	return exportmetric.DeltaExporter
 }
